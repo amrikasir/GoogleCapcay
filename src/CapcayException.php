2dbd8b77a73e0287af1389a3e9c683bc2496843a
dindaps <49602809+dindaps@users.noreply.github.com>
2019-04-14 11:23:12 +0700

    Update CapcayException.php
    mengubah variabel menjadi b.indo

--- src/CapcayException.php
+++ src/CapcayException.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Created by PhpStorm.
+ * Created by Dinda.
  * User: zero
  * Date: 5/22/17
  * Time: 10:08 AM
@@ -13,9 +13,9 @@ use Throwable;
 
 class CapcayException extends \Exception
 {
-	public function __construct($message = "", $code = 0, Throwable $previous = null)
+	public function __construct($pesan = "", $code = 0, Throwable $previous = null)
 	{
-		echo $message;
+		echo $pesan;
 	}
 
 }
