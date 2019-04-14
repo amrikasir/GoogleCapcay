848c3d44470b2bbeaffe1f3bbaa1f8c8bbcb0855
Rezapopi <49608212+Rezapopi@users.noreply.github.com>
2019-04-14 18:48:10 +0700

    Perbaikan pesan Error Excecption

--- src/GoogleCapcayRuntimeException.php
+++ src/GoogleCapcayRuntimeException.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Created by PhpStorm.
+ * Created by Reza.
  * User: zero
  * Date: 5/22/17
  * Time: 9:54 AM
@@ -15,7 +15,9 @@ class GoogleCapcayRuntimeException extends \RuntimeException
 
 	public function __construct($message = "", $code = 0, Throwable $previous = null)
 	{
-		echo $message . " : " . $code;
+		echo $message;
+		echo "<hr>";
+		echo $code;
 
 		return parent::__construct($message, $code, $previous);
 	}
